65a46f16cfc0d9bd6700100bb5d87e82e2d8fefa
mehmettberke <dxd3d9@gmail.com>
2024-11-24 12:42:06 +0300

    typo fix

--- src/app/news/[title]/page.tsx
+++ src/app/news/[title]/page.tsx
@@ -10,7 +10,7 @@ export default function NewsPage({ params }: { params: { title: string } }) {
     <div className="w-full sm:w-[1400px] mx-auto min-h-screen bg-white p-2 sm:p-8">
       <Navbar />
       <Categories />
-      <NewsDetail title={decodedTitle} />;
+      <NewsDetail title={decodedTitle} />
       <Footer />
     </div>
   );
